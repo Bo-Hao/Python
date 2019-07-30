824b98956078daae3b03b49042538c4e906f9202
Bo-Hao <bohaoperng@gmail.com>
2019-07-30 17:23:55 +0800

    20190730

--- py_file/Others/MDS.py
+++ py_file/Others/MDS.py
@@ -1,23 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 class MyMDS:
     def __init__(self, n_components):
         self.n_components = n_components
+    
+    def fit(self, q):
+        D = np.asarray(self.n_components)
+        D_square = D ** 2
+        total_Mean = np.mean(D_square)
+        column_Mean = np.mean(D_square, axis = 0)
+        row_Mean = np.mean(D_square, axis = 1)
+        B = [[-0.5 * (D_square[i][j] - row_Mean[i] - column_Mean[j] + total_Mean) 
+        for i in np.arange(D_square.shape[0])] for j in np.arange(D_square.shape[1])]
+        eigenValue, eigenVector = np.linalg.eig(B)
+        self.Z = np.dot(eigenVector[:, :q], np.sqrt(np.diag(eigenValue[:q])))
+    
+    def plot2D(self):
+        if self.Z.shape[1] == 2:
+            for i in np.arange(self.Z.shape[0]):
+                plt.scatter(self.Z[i][0], self.Z[i][1])
+            
+            plt.show()
+        else:
+            print("Not 2D")
+        
+        
 
-    def fit(self, data):
-        m, n = data.shape
-        dist = np.zeros((m, m))
-        dist = [np.sum(np.square(data[i] - data), axix = 1).reshape(1, m) for i in np.arange(m)]
-        disti = [np.mean(dist[i, :]) for i in np.arange(m)]
-        distj = [np.mean(dist[:, i]) for i in np.arange(m)]
-        distij = mp.mean(dist)
-        B = [[-0.5 * (dist[i,j] - disti[i] - distj[j] + distij) for i in np.arange(m)] for j in np.arange(m)]
-        lamda, V = np.linalg.eigh(B)
-        index = np.argsort(-lamda)[:self.n_components]
-        diag_lamda = np.sqrt(np.diag(-np.sort(-lamda)[:self.n_components]))
-        V_selected = V[:,index]
-        Z = V_selected.dot(diag_lamda)
-        return Z
-
-
+    
