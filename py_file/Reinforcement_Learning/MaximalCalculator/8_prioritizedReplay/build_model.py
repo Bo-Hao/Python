0f5389f650989bfb1cb7e8c1bb5b053aa8a9fecb
Bo-Hao <bohaoperng@gmail.com>
2019-08-28 20:35:12 +0800

    prioritized_replay

---
+++ py_file/Reinforcement_Learning/MaximalCalculator/8_prioritizedReplay/build_model.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import tensorflow as tf
+from NoisyDense import NoisyDense
+import copy
+
+class Build_Model():
+    def __init__(self, state_size, neurons, action_size):
+        self.state_size = state_size
+        self.neurons = neurons
+        self.action_size = action_size
+        self.lr = 0.1
+
+        state_input = tf.keras.layers.Input(shape=(self.state_size, ))
+        D1 = tf.keras.layers.Dense(self.neurons, activation='tanh')(state_input)
+
+        #連結層
+        d1 = tf.keras.layers.Dense(self.neurons,activation='tanh')(D1)
+        d2 = tf.keras.layers.Dense(self.neurons,activation='tanh')(d1)
+        #dueling
+        d3_a = tf.keras.layers.Dense(self.neurons/2, activation='sigmoid')(d2)
+        d3_v = tf.keras.layers.Dense(self.neurons/2, activation='sigmoid')(d2)
+        a = tf.keras.layers.Dense(self.action_size,activation='linear')(d3_a)
+        value = tf.keras.layers.Dense(1,activation='linear')(d3_v)
+        a_mean = tf.keras.layers.Lambda(lambda x: tf.keras.backend.mean(x, axis=1, keepdims=True))(a)
+        advantage = tf.keras.layers.Subtract()([a, a_mean])
+        q = tf.keras.layers.Add()([value, advantage])
+        # noisy
+        self.noise = NoisyDense(self.action_size)
+        final = self.noise(q)
+
+        #最後compile
+        self.model = tf.keras.models.Model(inputs=state_input, outputs=final)
+        self.model.compile(loss = 'mse', optimizer = tf.optimizers.Adam(lr = self.lr))
---
+++ py_file/Reinforcement_Learning/MaximalCalculator/8_prioritizedReplay/run.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+from env import Env
+from Brain import NoisyQ
+import numpy as np
+import tensorflow as tf 
+import copy
+
+
+def update():
+    for episode in range(300):
+        # initial observation
+        s = 0
+        E = Env()
+        while True:
+            # RL choose action based on observation
+            action = RL.choose_action(s)
+            # RL take action and get next observation and reward
+            s_, reward, done = E.step(action)
+            print(action, reward)
+            # RL learn from this transition
+            RL.learn(s, action, reward, s +2) # Multi-steps learning +2 so, N-2
+            # swap observation
+            s = s_
+            # break while loop when end of this episode
+            if done:
+                #RL.epsilon += 0.001
+                break
+        if episode %10 == 0:
+            RL.dump_model = copy.copy(RL.model)
+            
+        
+    E = Env()
+    print("---------------test---------------")
+    RL.m.bias_noisy = False
+    RL.m.weight_noisy = False
+    for i in range(E.final_step):
+        q_table = RL.model.predict([i])
+        E.step(np.argmax(q_table))
+        print(np.argmax(q_table))
+    print(E.score)
+
+if __name__ == "__main__":
+    env = Env()
+    RL = NoisyQ(actions=list(range(env.n_actions)))
+    update()
+    
+    
